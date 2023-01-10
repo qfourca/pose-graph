10341c6fe2b253dfbe1ccd88dd3ed5df22d5f50d
종박 <78944379+jongpark1234@users.noreply.github.com>
2023-01-10 14:14:43 +0900

    Graph Example

--- src/log/GraphLogger.tsx
+++ src/log/GraphLogger.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
 
 const data = {
     labels: [1, 2, 3, 4, 5, 6, 7],
@@ -8,13 +19,19 @@ const data = {
             label: 'A',
             backgroundColor: 'rgb(192, 192, 75)',
             borderColor: 'rgb(192, 192, 75)',
-            data: [1, 4, 9, 16, 25, 36, 49]
+            data: [1 ** 2, 2 ** 2, 3 ** 2, 4 ** 2, 5 ** 2, 6 ** 2, 7 ** 2]
         },
         {
             label: 'B',
             backgroundColor: 'rgb(75, 192, 192)',
             borderColor: 'rgb(75, 192, 192)',
-            data: [1, 8, 27, 64, 125, 216, 343]
+            data: [1 ** 3, 2 ** 3, 3 ** 3, 4 ** 3, 5 ** 3, 6 ** 3, 7 ** 3]
+        },
+        {
+            label: 'C',
+            backgroundColor: 'rgb(192, 75, 75)',
+            borderColor: 'rgb(192, 75, 75)',
+            data: [1 ** 4, 2 ** 4, 3 ** 4, 4 ** 4, 5 ** 4, 6 ** 4, 7 ** 4]
         },
     ]
 }
@@ -26,8 +43,12 @@ const options = {
         },
         title: {
             display: true,
-            text: 'Chart.js Line Chart'
+            text: 'Angle Graph'
         }
+    },
+    interaction: {
+        mode: 'index' as const,
+        intersect: false
     }
 }
 
