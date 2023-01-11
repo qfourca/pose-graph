17ce8399fb42bb4bb4bc72ddbd410bfe33934e8f
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-01-11 16:03:09 +0900

    size

--- src/log/GraphLogger.style.tsx
+++ src/log/GraphLogger.style.tsx
@@ -24,10 +24,15 @@ export const partContainer = styled.div<{isFocus: boolean, bkColor: string}>`
     background-color: ${({bkColor}) => bkColor};
     border-radius: .5em;
     cursor: pointer;
-    padding: .2em;
+    padding: 1em .3em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     > label {
+        width: 100%;
         cursor: pointer;
         padding: .2em;
+        font-size: .9em;
     }
     outline: ${({isFocus}) => isFocus ? 2 : 0}px solid red;
 `
